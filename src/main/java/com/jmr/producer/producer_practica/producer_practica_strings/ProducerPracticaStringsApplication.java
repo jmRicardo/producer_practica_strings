57a4e350db8a3536666c55496de76721e5f35c76
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-10 12:47:07 -0300

    nuevo servicio para testeo de Ribbon/Feign

---
+++ src/main/java/com/jmr/producer/producer_practica/producer_practica_strings/ProducerPracticaStringsApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.producer.producer_practica.producer_practica_strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerPracticaStringsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerPracticaStringsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/producer/producer_practica/producer_practica_strings/ProducerPracticaStringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.producer.producer_practica.producer_practica_strings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerPracticaStringsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
