fea45fc7b50a0c34dc600c85ed9e1b73c9d513e1
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-12 16:50:18 -0300

    Add support for bootstrap file

--- src/main/java/com/jmr/producer/producer_practica/producer_practica_strings/ProducerPracticaStringsApplication.java
+++ src/main/java/com/jmr/producer/producer_practica/producer_practica_strings/ProducerPracticaStringsApplication.java
@@ -2,10 +2,11 @@ package com.jmr.producer.producer_practica.producer_practica_strings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class ProducerPracticaStringsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/jmr/producer/producer_practica/producer_practica_strings/controllers/BrandController.java
+++ src/main/java/com/jmr/producer/producer_practica/producer_practica_strings/controllers/BrandController.java
@@ -2,17 +2,27 @@ package com.jmr.producer.producer_practica.producer_practica_strings.controllers
 
 import com.jmr.producer.producer_practica.producer_practica_strings.models.Brand;
 import com.jmr.producer.producer_practica.producer_practica_strings.services.BrandService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/brand")
 public class BrandController {
 
     private final BrandService brandService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     public BrandController(BrandService brandService) {
         this.brandService = brandService;
@@ -22,6 +32,17 @@ public class BrandController {
     public List<Brand> getAll()
     {
         return brandService.all();
+    }
+
+    @GetMapping("/get-config")
+    public ResponseEntity<?> obtenerConfig(){
+
+        log.info(texto);
+
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", texto);
+
+        return new ResponseEntity<>(json, HttpStatus.OK);
     }
 
 
