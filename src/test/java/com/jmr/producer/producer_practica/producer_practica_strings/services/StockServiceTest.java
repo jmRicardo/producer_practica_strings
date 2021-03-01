c2e9d349e0dd443253b1422b5f2b7015e4b476ce
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-03-01 15:24:34 -0300

    test findbyid de service creado

--- src/test/java/com/jmr/producer/producer_practica/producer_practica_strings/services/StockServiceTest.java
+++ src/test/java/com/jmr/producer/producer_practica/producer_practica_strings/services/StockServiceTest.java
@@ -2,19 +2,25 @@ package com.jmr.producer.producer_practica.producer_practica_strings.services;
 
 import com.jmr.practica.entities_practica.libreria_custom_maven_practica.models.entities.Stock;
 import com.jmr.producer.producer_practica.producer_practica_strings.repositories.StockRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.*;
 
 class StockServiceTest {
+
+    private static final long idStock = 1;
 
     @Mock
     StockRepository stockRepository;
@@ -27,16 +33,30 @@ class StockServiceTest {
         openMocks(this);
     }
 
-
     @Test
-    void createStockTest() {
+    void createStock() {
         Stock stock = mock(Stock.class);
+
         stockService.createStock(stock);
+
         verify(stockRepository,times(1)).save(stock);
     }
 
     @Test
-    void allTest() {
+    void findById() {
+        Stock stock = mock(Stock.class);
+
+        when(stockRepository.findById(idStock)).thenReturn(Optional.ofNullable(stock));
+
+        Stock data = stockService.findById(idStock);
+
+        verify(stockRepository,times(1)).findById(idStock);
+
+        assertEquals(stock,data);
+    }
+
+    @Test
+    void all() {
 
         List<Stock> stock = Collections.emptyList();
 
@@ -49,10 +69,6 @@ class StockServiceTest {
         verify(stockRepository, times(1)).findAll();
     }
 
-
-    @Test
-    void findById() {
-    }
 
     @Test
     void findByBrandModel() {
