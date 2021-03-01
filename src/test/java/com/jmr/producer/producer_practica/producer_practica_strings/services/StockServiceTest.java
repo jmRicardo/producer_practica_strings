a1690aa906ff29a46de6882b5f98d6c4f93bcb34
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-03-01 15:27:25 -0300

    test idnotfound de service creado

--- src/test/java/com/jmr/producer/producer_practica/producer_practica_strings/services/StockServiceTest.java
+++ src/test/java/com/jmr/producer/producer_practica/producer_practica_strings/services/StockServiceTest.java
@@ -1,26 +1,24 @@
 package com.jmr.producer.producer_practica.producer_practica_strings.services;
 
 import com.jmr.practica.entities_practica.libreria_custom_maven_practica.models.entities.Stock;
+import com.jmr.producer.producer_practica.producer_practica_strings.exceptions.StockNotFound;
 import com.jmr.producer.producer_practica.producer_practica_strings.repositories.StockRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.*;
 
 class StockServiceTest {
 
-    private static final long idStock = 1;
+    private static final long ID_STOCK = 1;
 
     @Mock
     StockRepository stockRepository;
@@ -34,7 +32,8 @@ class StockServiceTest {
     }
 
     @Test
-    void createStock() {
+    void createStock()
+    {
         Stock stock = mock(Stock.class);
 
         stockService.createStock(stock);
@@ -43,21 +42,29 @@ class StockServiceTest {
     }
 
     @Test
-    void findById() {
+    void findByIdNotFound()
+    {
+        when(stockRepository.findById(ID_STOCK)).thenReturn(Optional.empty());
+        assertThrows(StockNotFound.class, () -> stockService.findById(ID_STOCK));
+    }
+
+    @Test
+    void findById()
+    {
         Stock stock = mock(Stock.class);
 
-        when(stockRepository.findById(idStock)).thenReturn(Optional.ofNullable(stock));
+        when(stockRepository.findById(ID_STOCK)).thenReturn(Optional.ofNullable(stock));
 
-        Stock data = stockService.findById(idStock);
+        Stock data = stockService.findById(ID_STOCK);
 
-        verify(stockRepository,times(1)).findById(idStock);
+        verify(stockRepository,times(1)).findById(ID_STOCK);
 
         assertEquals(stock,data);
     }
 
     @Test
-    void all() {
-
+    void all()
+    {
         List<Stock> stock = Collections.emptyList();
 
         when(stockRepository.findAll()).thenReturn(stock);
